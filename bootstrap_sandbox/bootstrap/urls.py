54bba4fe4b2ad6ab779dd3cdaa5994b7a99bd63e
Arthur234 <ayatsun@gmail.com>
2019-09-02 15:51:43 +0300

    typography

--- bootstrap_sandbox/bootstrap/urls.py
+++ bootstrap_sandbox/bootstrap/urls.py
@@ -1,1 +1,8 @@
-urlpattern = []
+from django.urls import path
+
+from .views import home_view
+
+
+urlpatterns = [
+    path('', home_view)
+]
--- bootstrap_sandbox/bootstrap/views.py
+++ bootstrap_sandbox/bootstrap/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def typography_view(request):
+    return render(request, 'basic_typography.html')
